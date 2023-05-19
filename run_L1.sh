ec02ac5a0a7b8a5c7dca3763306fdf1ad305da3d
Michael Neuling <mikey@neuling.org>
2023-05-19 13:24:43 +1000

    reduce artifact size

--- run_L1.sh
+++ run_L1.sh
@@ -30,6 +30,6 @@ output/build/host-qemu-custom/build/qemu-system-ppc64 -nographic \
   -display none -vga none -m 4G -accel tcg,thread=multi \
   -serial mon:stdio \
   -smp cores=$N,maxcpus=$N,threads=1 \
-  -kernel output/images/vmlinux \
-  -initrd output/images/rootfs.cpio \
+  -kernel overlay/vmlinux \
+  -initrd overlay/rootfs.cpio \
   -virtfs local,path=overlay,mount_tag=host0,security_model=mapped-xattr,id=host0
